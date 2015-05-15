d45d2a6e49746c1762ed41c11bc8f26201a3d2d4
Abid Famasya <abid.famasya@gmail.com>
2015-05-16 00:23:47 +0700

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -54,13 +54,9 @@ class recommender:
         self.username2id = {}
         self.userid2name = {}
         self.productid2name = {}
-        # for some reason I want to save the name of the metric
         self.metric = metric
         if self.metric == 'pearson':
             self.fn = self.pearson
-        #
-        # if data is dictionary set recommender data to it
-        #
         if type(data).__name__ == 'dict':
             self.data = data
 
@@ -80,7 +76,6 @@ class recommender:
         ratings = list(ratings.items())
         ratings = [(self.convertProductID2name(k), v)
                    for (k, v) in ratings]
-        # finally sort and return
         ratings.sort(key=lambda artistTuple: artistTuple[1],
                      reverse = True)
         ratings = ratings[:n]
@@ -95,13 +90,9 @@ class recommender:
         located"""
         self.data = {}
         i = 0
-        #
-        # First load book ratings into self.data
-        #
         f = codecs.open(path + "BX-Book-Ratings.csv", 'r', 'utf8')
         for line in f:
             i += 1
-            #separate line into fields
             fields = line.split(';')
             user = fields[0].strip('"')
             book = fields[1].strip('"')
@@ -113,14 +104,11 @@ class recommender:
             currentRatings[book] = rating
             self.data[user] = currentRatings
         f.close()
-        #
-        # Now load books into self.productid2name
         # Books contains isbn, title, and author among other fields
         #
         f = codecs.open(path + "BX-Books.csv", 'r', 'utf8')
         for line in f:
             i += 1
-            #separate line into fields
             fields = line.split(';')
             isbn = fields[0].strip('"')
             title = fields[1].strip('"')
@@ -128,10 +116,6 @@ class recommender:
             title = title + ' by ' + author
             self.productid2name[isbn] = title
         f.close()
-        #
-        #  Now load user info into both self.userid2name and
-        #  self.username2id
-        #
         f = codecs.open(path + "BX-Users.csv", 'r', 'utf8')
         for line in f:
             i += 1
@@ -173,7 +157,6 @@ class recommender:
                 sum_y2 += pow(y, 2)
         if n == 0:
             return 0
-        # now compute denominator
         denominator = (sqrt(sum_x2 - pow(sum_x, 2) / n)
                        * sqrt(sum_y2 - pow(sum_y, 2) / n))
         if denominator == 0:
@@ -191,36 +174,25 @@ class recommender:
                 distance = self.fn(self.data[username],
                                    self.data[instance])
                 distances.append((instance, distance))
-        # sort based on distance -- closest first
-        distances.sort(key=lambda artistTuple: artistTuple[1],
+\        distances.sort(key=lambda artistTuple: artistTuple[1],
                        reverse=True)
         return distances
 
     def recommend(self, user):
        """Give list of recommendations"""
        recommendations = {}
-       # first get list of users  ordered by nearness
        nearest = self.computeNearestNeighbor(user)
-       #
-       # now get the ratings for the user
        #
        userRatings = self.data[user]
        #
-       # determine the total distance
        totalDistance = 0.0
        for i in range(self.k):
           totalDistance += nearest[i][1]
        # now iterate through the k nearest neighbors
-       # accumulating their ratings
        for i in range(self.k):
-          # compute slice of pie 
           weight = nearest[i][1] / totalDistance
-          # get the name of the person
           name = nearest[i][0]
-          # get the ratings for this person
           neighborRatings = self.data[name]
-          # get the name of the person
-          # now find bands neighbor rated that user didn't
           for artist in neighborRatings:
              if not artist in userRatings:
                 if artist not in recommendations:
@@ -230,11 +202,9 @@ class recommender:
                    recommendations[artist] = (recommendations[artist]
                                               + neighborRatings[artist]
                                               * weight)
-       # now make list from dictionary
        recommendations = list(recommendations.items())
        recommendations = [(self.convertProductID2name(k), v)
                           for (k, v) in recommendations]
-       # finally sort and return
        recommendations.sort(key=lambda artistTuple: artistTuple[1],
                             reverse = True)
        # Return the first n items
